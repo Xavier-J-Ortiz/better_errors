7528ea92cda8a360ae764d506fb7d7455b40f3b1
xortiz <xavier.ortiz.ch@gmail.com>
2018-02-25 10:47:02 -0500

    comments to pertinent places where the changes should happen.

--- lib/better_errors/error_page.rb
+++ lib/better_errors/error_page.rb
@@ -2,13 +2,16 @@ require "cgi"
 require "json"
 require "securerandom"
 
+# creates the errors, passes them along to the error page.
+
 module BetterErrors
   # @private
   class ErrorPage
+    #finds the path to the template to add error information to
     def self.template_path(template_name)
       File.expand_path("../templates/#{template_name}.erb", __FILE__)
     end
-
+    #finds the path to the template to add error information to
     def self.template(template_name)
       Erubi::Engine.new(File.read(template_path(template_name)), escape: true)
     end
@@ -25,11 +28,11 @@ module BetterErrors
     def id
       @id ||= SecureRandom.hex(8)
     end
-
+    # renders a template
     def render(template_name = "main")
       binding.eval(self.class.template(template_name).src)
     end
-
+    # I believe this shows the variables used within a page
     def do_variables(opts)
       index = opts["index"].to_i
       @frame = backtrace_frames[index]
@@ -70,7 +73,7 @@ module BetterErrors
       application_frames.first || backtrace_frames.first
     end
 
-  private
+    private
     def editor_url(frame)
       BetterErrors.editor[frame.filename, frame.line]
     end
@@ -102,7 +105,7 @@ module BetterErrors
     def text_heading(char, str)
       str + "\n" + char*str.size
     end
-
+    # inspects the value of an object
     def inspect_value(obj)
       inspect_raw_value(obj)
     rescue NoMethodError
@@ -110,7 +113,9 @@ module BetterErrors
     rescue Exception => e
       "<span class='unsupported'>(exception #{CGI.escapeHTML(e.class.to_s)} was raised in inspect)</span>"
     end
-
+    #determines the value to return to the error page for a given object being inspected.
+    # depending on the size of the variable error message, if the variable should be passed or if we should
+    # suggest that the variable be shortened
     def inspect_raw_value(obj)
       value = CGI.escapeHTML(obj.inspect)
 
@@ -122,7 +127,8 @@ module BetterErrors
           "or increase BetterErrors.maximum_variable_inspect_size)</span>"
       end
     end
-
+    #determines if a value is small enough to inspect. Currently using the length of the value 
+    #as the indicator if it should be shortened or not.
     def value_small_enough_to_inspect?(value)
       return true if BetterErrors.maximum_variable_inspect_size.nil?
       value.length <= BetterErrors.maximum_variable_inspect_size
